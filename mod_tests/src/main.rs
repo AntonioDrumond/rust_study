ab2ca26b2fb4a75785f49104f650d9d92fcf3044
Antonio Drumond <adcdstom@gmail.com>
2025-04-17 16:43:35 -0300

    scoped

--- mod_tests/src/main.rs
+++ mod_tests/src/main.rs
@@ -1,19 +1,45 @@
-mod other; 
+mod other; // including external file
 
 mod m1 {
-    pub const A:i32 = 120;
-    mut var = 20;
+    pub const _A:i32 = 120;
+    const _B:i32 = 30;
+
     pub fn func1(){
         println!("func1 called");
     }
+
+    pub fn get_b() -> i32{
+        _B
+    }
+}
+
+mod m3 {
+    use super::m1::*;
+
+    pub fn f1(){
+        println!("A = {_A}");
+    }
+
+    /* // Wont work bc _B is private
+    pub fn f2(){
+        println!("B = {_B}");
+    }
+    */
 }
 
 fn main() {
     println!("Inicio do programa");
 
     m1::func1();
+
     other::func();
 
-    m1::var = 202;
-    println!("{m1::var}");
+    other::m2::func();
+
+    println!("\nTrying to get not public constant");
+    let a = m1::get_b();
+    println!("a = {a}");
+
+    println!("\nSuper tests");
+    m3::f1();
 }
--- mod_tests/src/other.rs
+++ mod_tests/src/other.rs
@@ -1,3 +1,9 @@
+pub mod m2{
+    pub fn func(){
+        println!("func from m2 called");
+    }
+}
+
 pub fn func(){
     println!("inAnotherFile called");
 }
